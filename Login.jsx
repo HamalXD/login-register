3ef267ac1514a64053bde0e615920ac54886f534
Hamal <117851589+HamalXD@users.noreply.github.com>
2023-01-05 12:59:53 +0545

    login-register

---
+++ Login.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+export const Login = (props) => {
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email);
+        alert ('Your logged in')
+    }
+
+    return (
+        <div className="auth-form-container">
+            <h2>Login</h2>
+            <form className="login-form" onSubmit={handleSubmit}>
+                <label>EMAIL</label>
+                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" id="email" name="email" />
+                <label>PASSWORD</label>
+                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" id="password" name="password" />
+                <button type="submit">LOG IN</button>
+            </form>
+            <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
+        </div>
+    )
+}
